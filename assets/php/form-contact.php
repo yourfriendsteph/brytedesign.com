d1e02383a01a3d4732d58a1cbf33ea45bb4c9970
Alex Elman <aelman@indeed.com>
2020-09-08 19:33:33 -0500

    Use tls

--- assets/php/form-contact.php
+++ assets/php/form-contact.php
@@ -94,7 +94,7 @@ $mail->Host       = 'email-smtp.us-west-2.amazonaws.com';   // Set the SMTP serv
 $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
 $mail->Username   = 'AKIAXFSLSILZV4WV2LVL';                 // SMTP username
 $mail->Password   = 'secret';                               // SMTP password
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+$mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
 // Content
